be9018c6c56fc0998c7542de2d7cea3ef3b9162a
Paweł Biernat <pawel.biernat@gmail.com>
2014-04-25 16:36:04 +0200

    Initial commit

--- src/InterPol.jl
+++ src/InterPol.jl
@@ -1,5 +1,64 @@
 module InterPol
 
-# package code goes here
+export interpolateAt, interpolateDerivativeAt
+
+# returns the value of the interpolation polynomial at the point x0
+function interpolateAt{T<:Number,N}(x::Vector{T}, y::AbstractArray{T,N}, x0::T)
+
+    if length(x)!=size(y,1)
+        error("x and y have to be of the same size.")
+    end
+
+    n = length(x)
+    p = zero(T)
+
+    for i=1:n
+        Li =one(T)
+        dLi=zero(T)
+        for j=1:n
+            if j==i
+                continue
+            else
+                Li*=(x0-x[j])/(x[i]-x[j])
+            end
+        end
+        p.+=Li.*y[i, [1:size(y,n) for n=2:N]...]
+    end
+    return p
+end
+
+# returns the value of the derivative of the interpolation polynomial
+# at the point x0
+function interpolateDerivativeAt{T<:Number,N}(x::Vector{T}, y::AbstractArray{T,N}, x0::T)
+
+    if length(x)!=size(y,1)
+        error("x and y have to be of the same size.")
+    end
+
+    n = length(x)
+    p = zero(T)
+
+    for i=1:n
+        dLi=zero(T)
+        for k=1:n
+            if k==i
+                continue
+            else
+                dLi1=one(T)
+                for j=1:n
+                    if j==k || j==i
+                        continue
+                    else
+                        dLi1*=(x0-x[j])/(x[i]-x[j])
+                    end
+                end
+                dLi+=dLi1/(x[i]-x[k])
+            end
+        end
+        p.+=dLi.*y[i, [1:size(y,n) for n=2:N]...]
+        # p+=dLi*y[i]
+    end
+    return p
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,32 @@
 using InterPol
-using Base.Test  
+using Base.Test
 
-# write your own tests here
-@test 1 == 1
+@test interpolateAt([0], [0], 1) == 0
+
+@test interpolateAt([0], [1], 1) == 1
+
+@test interpolateAt([0,1,2,3], [0,1,2,3], 0) == 0
+
+@test interpolateAt([0,1,2,3], [0,1,2,3], 4) == 4
+
+@test interpolateAt([0,1,2], [0,1,4], 0) == 0
+
+@test interpolateAt([0,1,2], [0,1,4], 1) == 1
+
+@test interpolateAt([0,1,2], [0,1,4], 3) == 9
+
+
+
+@test interpolateDerivativeAt([0], [0], 1) == 0
+
+@test interpolateDerivativeAt([0], [1], 1) == 0
+
+@test interpolateDerivativeAt([0,1,2,3], [0,1,2,3], 0) == 1
+
+@test interpolateDerivativeAt([0,1,2,3], [0,1,2,3], 4) == 1
+
+@test interpolateDerivativeAt([0,1,2], [0,1,4], 0) == 0
+
+@test interpolateDerivativeAt([0,1,2], [0,1,4], 1) == 2
+
+@test interpolateDerivativeAt([0,1,2], [0,1,4], 3) == 6
